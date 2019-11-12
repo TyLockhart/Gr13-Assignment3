42c407929682aa7e0924bbedccfe0aa0017e0407
TyLockhart <53748468+TyLockhart@users.noreply.github.com>
2019-11-12 10:59:42 -0500

    Nov12, 2019

--- Assignment3/main.cpp
+++ Assignment3/main.cpp
@@ -1,5 +1,5 @@
-//work o nsetting paddle same x as ball
-
+//!!work o nsetting paddle same x as ball
+//work on creating menu bricks correctly
 
 #include "physics.h"
 #include <vector>
@@ -17,9 +17,12 @@ int main()
 //Declares
     vector<RectangleShape> Menu_Background;
     vector<Vertex> Menu_Vertex;
-    vector<Block> Menu_Wall;
+    vector<Block> Menu_Wall,Menu_Brick;
+    vector<Vector2f> Menu_BrickPoints;
 
     Clock Menu_Clock;
+
+    Color Menu_BrickColor(Color(243,156,18,255));
 
 //Bools
     bool Menu=true;
@@ -33,6 +36,17 @@ int main()
     int Menu_VxR=142;
     int Menu_VxG=68;
     int Menu_VxB=173;
+    int Menu_BrickSpaceX=0;
+    int Menu_BrickSpaceY=0;
+
+//Points
+    Menu_BrickPoints.push_back(Vector2f(0,0));
+    Menu_BrickPoints.push_back(Vector2f(40,6));
+    Menu_BrickPoints.push_back(Vector2f(80,0));
+    Menu_BrickPoints.push_back(Vector2f(80,40));
+    Menu_BrickPoints.push_back(Vector2f(40,34));
+    Menu_BrickPoints.push_back(Vector2f(0,40));
+    Menu_BrickPoints.push_back(Vector2f(0,0));
 
     RenderWindow window(VideoMode(800,800),"Assignment3: Brick Breaker");
     window.setFramerateLimit(60);
@@ -79,9 +93,28 @@ int main()
             Menu_Wall.push_back(physics::createBox(World_Menu,0,800,800,1,b2_staticBody));
             physics::setBlockColor(Menu_Wall.back(),Color::Transparent);//3 Wall bottom
 
+        //Creating bricks
+        for(int i=0; i<=8; i++)
+        {
+//        Menu_Brick.push_back(physics::createBox(World_Menu,25+Menu_BrickSpaceX,25+Menu_BrickSpaceY,65,35,b2_staticBody));
+//        physics::setBlockColor(Menu_Brick.back(),Menu_BrickColor);
+        Menu_Brick.push_back(physics::createPolygon(World_Menu,26+Menu_BrickSpaceX,25+Menu_BrickSpaceY,Menu_BrickPoints,b2_staticBody));
+        physics::setBlockColor(Menu_Brick.back(),Menu_BrickColor);
+
+        Menu_BrickSpaceX+=95;
+
+        if(i==8&&Menu_BrickSpaceY<=240)
+        {
+            i=0;
+            Menu_BrickSpaceX=0;
+            Menu_BrickSpaceY+=60;
+        }
+        }
+        //End of creating bricks
+
             Menu_Once=false;
         }
-
+        //Menu background gradient
             Menu_Vertex.clear();
             Menu_Vertex.push_back(Vertex(Vector2f(0,0),Color(Menu_VxR,Menu_VxG,Menu_VxB)));
             Menu_Vertex.push_back(Vertex(Vector2f(800,0),Color(Menu_VxR,Menu_VxG,Menu_VxB)));
@@ -113,32 +146,45 @@ int main()
             Menu_CountUp=false;
             Menu_CountDown=true;
         }
+        //End gradient
 
-        Menu_Paddle->setPosition(Vector2f(Menu_Ball->getPosition().x,700));
+        for(int i=0;i<Menu_Brick.size();i++){
+        if(physics::checkCollision(Menu_Brick[i])==true)
+        {
+
+
+
+            if(physics::isDestroyed(Menu_Brick[i])!=true){
+            physics::deleteBlock(World_Menu,Menu_Brick[i]);
+            Menu_Brick.erase(Menu_Brick.begin()+i);
+            }
+        }
+        }
 
 
 
 
-//        if(Menu_Paddle->getPosition().x<=0)
-//        {
-//            Menu_PaddleRight=false;
-//            Menu_PaddleLeft=true;
-//        }
-//        if(Menu_Paddle->getPosition().x>=800)
-//        {
-//            Menu_PaddleLeft=false;
-//            Menu_PaddleRight=true;
-//        }
-//        if(Menu_PaddleLeft==true) Menu_Paddle->setVelocity(Vector2f(60,0));
-//        if(Menu_PaddleRight==true) Menu_Paddle->setVelocity(Vector2f(-60,0));
 
-    if(Keyboard::isKeyPressed(Keyboard::A)==true)
+
+
+
+    //Paddle following ball
+    if(Menu_Ball->getPosition().x>Menu_Paddle->getPosition().x&&Menu_Ball->getPosition().y<700)
     {
-        cout<<"test"<<endl;
+        Menu_Paddle->setVelocity(Vector2f(280,0));
     }
+    else if(Menu_Ball->getPosition().x<Menu_Paddle->getPosition().x&&Menu_Ball->getPosition().y<700)
+    {
+        Menu_Paddle->setVelocity(Vector2f(-280,0));
+    }
+    //End paddle following ball
 
-        window.draw(&Menu_Vertex[0],Menu_Vertex.size(),TrianglesFan);
+
+
+
+        window.draw(&Menu_Vertex[0],Menu_Vertex.size(),TrianglesFan);//Background gradient
         for(auto i:Menu_Background) window.draw(i);
+//        for(auto i:Menu_Brick) window.draw(i);
         physics::displayWorld(World_Menu,window);
 //        window.draw(*Menu_Paddle);
         Menu_Paddle->updatePosition();
